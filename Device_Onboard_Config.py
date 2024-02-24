47688a32d24fed005d7da0da64ee288bc84b23b2
daisy-dynawhite <daledynawhite@outlook.com>
2024-02-24 14:53:58 +0200

    Update names

---
+++ Device_Onboard_Config.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+#Import libraries
+from netmiko import ConnectHandler
+from jinja2 import Environment, FileSystemLoader
+import getpass, yaml
+
+#Capture admin pword
+pword = getpass.getpass()
+
+#Declare template variables
+environment = Environment(loader=FileSystemLoader("."))
+template = environment.get_template("Device_Template.jinja2")
+
+with open('Device_Vars.yaml', 'r') as vars:
+	lvars = yaml.safe_load(vars)
+
+#Render config text via Jinja2 template
+output = template.render(lvars=lvars)
+
+#Save config to file
+fname = lvars['hostname']+".txt"
+with open (fname, mode="w", encoding="utf-8") as message:
+	message.write(output)
